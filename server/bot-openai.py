d5b66bfb246ced754d3dfab5026ffa369f46c163
abdulrahimchotu <rahim.s@consultadd.com>
2026-01-12 13:36:02 +0530

    added ice servers

--- server/bot-openai.py
+++ server/bot-openai.py
@@ -30,6 +30,7 @@ import os
 from dotenv import load_dotenv
 from loguru import logger
 from PIL import Image
+from aiortc import RTCIceServer
 from pipecat.audio.turn.smart_turn.local_smart_turn_v3 import LocalSmartTurnAnalyzerV3
 from pipecat.audio.vad.silero import SileroVADAnalyzer
 from pipecat.audio.vad.vad_analyzer import VADParams
@@ -216,7 +217,16 @@ async def bot(runner_args: RunnerArguments):
             )
         case SmallWebRTCRunnerArguments():
             webrtc_connection: SmallWebRTCConnection = runner_args.webrtc_connection
+            
+            webrtc_connection.ice_servers = [
+                RTCIceServer(urls="stun:stun.l.google.com:19302"),
+                RTCIceServer(urls="stun:stun1.l.google.com:19302"),
+                RTCIceServer(urls="stun:stun2.l.google.com:19302"),
+            ]
 
+            print(f"webrtc_connection ice_servers set with {len(webrtc_connection.ice_servers)} servers")
+            
+            
             transport = SmallWebRTCTransport(
                 webrtc_connection=webrtc_connection,
                 params=TransportParams(
